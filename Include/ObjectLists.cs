809f8a769226785f6837bacc214ca30fba1de310
KAZAK <dnsservice0309@gmail.com>
2025-10-18 18:10:47 +0400

    Доработка после AI-ревью

--- Include/ObjectLists.cs
+++ Include/ObjectLists.cs
@@ -1,10 +1,51 @@
-﻿namespace OBED.Include
+﻿using System.Collections.Concurrent;
+
+namespace OBED.Include
 {
     class ObjectLists
     {
-        public static List<Buffet> Buffets { get; set; } = [];
-        public static List<Canteen> Canteens { get; set; } = [];
-        public static List<Grocery> Groceries { get; set; } = [];
-        public static List<Person> Persons { get; set; } = [];
-    }
+        public static List<Buffet> Buffets { get; private set; } = [];
+        public static List<Canteen> Canteens { get; private set; } = [];
+        public static List<Grocery> Groceries { get; private set; } = [];
+		public static ConcurrentDictionary<long, Person> Persons { get; private set; } = [];
+
+		/// <summary>
+		/// Добавляет к общей базе новые точки или учётки.
+		/// </summary>
+		/// <param name="values">Лист с новыми точками/учётками.</param>
+		/// <exception cref="ArgumentException">Ошибки.</exception>
+		public static void AddRangeList<T>(List<T> values)
+        {
+			switch (values)
+			{
+				case (List<Buffet> buffets):
+					{
+						Buffets.AddRange(buffets);
+						break;
+					}
+				case (List<Canteen> canteens):
+					{
+						Canteens.AddRange(canteens);
+						break;
+					}
+				case (List<Grocery> groceries):
+					{
+						Groceries.AddRange(groceries);
+						break;
+					}
+				case (List<Person> persons):
+					{
+						foreach(var person in persons)
+						{
+							Persons.TryAdd(person.UserID, person);
+						}
+						break;
+					}
+				default:
+					{
+						throw new ArgumentException("Попытка присвоить неизвестный тип", nameof(values));
+					}
+			}
+		}
+	}
 }
--- Include/Person.cs
+++ Include/Person.cs
@@ -3,32 +3,54 @@
 	public enum RoleType
 	{
 		Administrator,
-		Common_User,
-		VIP_User
+		CommonUser,
+		VipUser
 	}
-	class Person(string username, long userID, RoleType role)
+	class Person
 	{
-		public string Username { get; private set; } = username;
-		public long UserID { get; init; } = userID;
-		public RoleType Role { get; private set; } = role;
+		public string Username { get; private set; }
+		public long UserID { get; init; }
+		public RoleType Role { get; private set; }
+		
+		public Person(string username, long userID, RoleType role)
+		{
+			if (userID <= 0)
+				throw new ArgumentException("UserID должно быть больше 0", nameof(userID));
+			if (string.IsNullOrWhiteSpace(username))
+				throw new ArgumentException("Username не может быть пустым или нулевым.", nameof(username));
+			if (!Enum.IsDefined(typeof(RoleType), role))
+				throw new ArgumentException("Недопустимое значение роли.", nameof(role));
 
+			Username = username;
+			UserID = userID;
+			Role = role;
+		}
 		// TODO: ChangeUsername()
 		// TODO: SetRole()
 	}
+	/// <summary>
+	/// Текущий тип обработки сообщений в чате от юзера. Без данных тегов UserState у сообщения игнорируются
+	/// </summary>
 	enum UserAction
 	{
+		/// <summary> Для НОВЫХ отзывов. Запрос рейтинга от 1 до 10</summary>
 		RatingRequest,
+		/// <summary> Для ОБНОВЛЕНИЯ отзывов. Запрос нового рейтинга от 1 до 10</summary>
 		RatingChange,
+		/// <summary> Для НОВЫХ отзывов. Запрос не-пустой строки</summary>
 		CommentRequest,
+		/// <summary> Для ОБНОВЛЕНИЯ отзывов. Запрос новой не-пустой строки</summary>
 		CommentChange,
+		/// <summary> Для НОВЫХ отзывов. Отметка, позволяющая перейти к финальному этапу отправки отзыва</summary>
 		NoActiveRequest,
+		/// <summary> Для ОБНОВЛЕНИЯ отзывов. Отметка, позволяющая перейти к финальному этапу обновления отзыва</summary>
 		NoActiveChange
 	}
-	class UserState()
+	class UserState
 	{
 		public UserAction? Action { get; set; }
-		public string? RefTo { get; set; }
-		public int Rating { get; set; }
+		public string? ReferenceToPlace { get; set; }
 		public string? Comment { get; set; }
+		public int Rating { get; set; }
 	}
 }
